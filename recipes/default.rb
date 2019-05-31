48f0760a817bc9766ddaa15542cf7780e192e7ca
vagrant <vagrant@vagrant.vm>
2019-05-31 20:50:22 +0000

    First commit

---
+++ recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook:: mynginx
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+package 'git'
+package 'tree'
+
+package 'nginx' do
+  action :install
+end
+
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
+
+template "/etc/nginx/nginx.conf" do   
+  source "nginx.conf.erb"
+  notifies :reload, "service[nginx]"
+end
+
+cookbook_file "/home/vagrant/nameworks.txt" do
+  source "names.txt"
+  mode "0644"
+end
+
